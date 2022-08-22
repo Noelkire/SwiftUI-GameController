4e2627a585a33262898c4e23d26ef6a82198ae30
Erik Leon <erikgleon@gmail.com>
2022-08-21 17:06:23 -0700

    Initial Commit

---
+++ JoyStickUI/JoyStickUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoyStickUIApp.swift
+//  JoyStickUI
+//
+//  Created by Erik Leon on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct JoyStickUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
